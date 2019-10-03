2ad5be053b777a63550174b9f45719c44e12c3c3
Benoit Koenig <koenigbenoit47@gmail.com>
2019-10-03 12:16:53 +0800

    Try a new 2d model

--- classification_2D/model.py
+++ classification_2D/model.py
@@ -3,34 +3,27 @@ from keras.models import Model
 
 from intracranial_hemorrhage_detection.classification_2D.params import input_image_size
 
+def get_bloc(input, nb_channels):
+    x = Conv2D(nb_channels, (3, 3), padding="same", activation="relu", kernel_initializer="he_uniform")(input)
+    x = Conv2D(nb_channels, (3, 3), padding="same", activation="relu", kernel_initializer="he_uniform")(x)
+    x = Conv2D(nb_channels * 2, (3, 3), padding="same", activation="relu", kernel_initializer="he_uniform")(x)
+    x = MaxPool2D()(x)
+    return x
+
 def get_model():
     "Returns the classifier model"
 
     input = Input(shape=(input_image_size, input_image_size, 1))
 
-    x = Conv2D(64, (3, 3), padding="same", activation="relu", kernel_initializer="he_uniform")(input)
-    x = Conv2D(64, (3, 3), padding="same", activation="relu", kernel_initializer="he_uniform")(x)
-    x = Conv2D(64, (3, 3), padding="same", activation="relu", kernel_initializer="he_uniform")(x)
-    x = MaxPool2D()(x)
-
-    x = Conv2D(128, (3, 3), padding="same", activation="relu", kernel_initializer="he_uniform")(x)
-    x = Conv2D(128, (3, 3), padding="same", activation="relu", kernel_initializer="he_uniform")(x)
-    x = Conv2D(256, (3, 3), padding="same", activation="relu", kernel_initializer="he_uniform")(x)
-    x = MaxPool2D()(x)
-
-    x = Conv2D(256, (3, 3), padding="same", activation="relu", kernel_initializer="he_uniform")(x)
-    x = Conv2D(256, (3, 3), padding="same", activation="relu", kernel_initializer="he_uniform")(x)
-    x = Conv2D(512, (3, 3), padding="same", activation="relu", kernel_initializer="he_uniform")(x)
-    x = MaxPool2D()(x)
-
-    x = Conv2D(512, (3, 3), padding="same", activation="relu", kernel_initializer="he_uniform")(x)
-    x = Conv2D(512, (3, 3), padding="same", activation="relu", kernel_initializer="he_uniform")(x)
-    x = Conv2D(1024, (3, 3), padding="same", activation="relu", kernel_initializer="he_uniform")(x)
-    x = MaxPool2D()(x)
+    x = get_bloc(input, 64)
+    x = get_bloc(x, 128)
+    x = get_bloc(x, 256)
+    x = get_bloc(x, 512)
+    x = get_bloc(x, 1024)
+    x = get_bloc(x, 2048)
 
     x = Flatten()(x)
     x = Dense(1024, activation='relu', kernel_initializer="he_uniform")(x)
-    x = Dense(1024, activation='linear', kernel_initializer="he_uniform")(x)
     predictions = Dense(6, activation='sigmoid')(x)
 
     return Model(inputs=input, outputs=predictions)
--- classification_2D/train.py
+++ classification_2D/train.py
@@ -10,7 +10,7 @@ def train_classifier():
     callbacks = [
         ModelCheckpoint(model_weights_path),
         CSVLogger("%s/outputs/classifier_2D_training_logs.csv" % folder_path),
-        LearningRateScheduler(lambda epoch, _: 1e-5 if (epoch < 60) else 1e-6),
+        LearningRateScheduler(lambda epoch, _: 1e-5 if (epoch < 200) else 1e-6),
     ]
     model = get_model()
     model.compile(optimizer=Adam(), loss="binary_crossentropy")
