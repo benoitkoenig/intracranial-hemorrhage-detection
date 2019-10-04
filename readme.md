ae848880a5f0c409e5241542f6861e717a453aac
Benoit Koenig <koenigbenoit47@gmail.com>
2019-10-04 19:27:41 +0800

    Update show.loss_graph

--- readme.md
+++ readme.md
@@ -16,7 +16,7 @@ Author: Benoît Koenig
 
 - Visualize a single dicom file: python -m show.dicom_file [stage_1_train|stage_1_test] [index]
 
-- Visualize the evolution of the loss for the 2D or 3D classifier: python -m show.loss_graph [2|3]
+- Visualize the evolution of the loss for the 2D or 3D classifier: python -m show.loss_graph [2D|3D]
 
 - Visualize the 3D reconstruction for the 3D classifier: python -m show.reconstruction_3D
 
--- show/loss_graph.py
+++ show/loss_graph.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import sys
 
@@ -6,22 +7,14 @@ from intracranial_hemorrhage_detection.constants import folder_path
 
 pd.plotting.register_matplotlib_converters()
 
-def get_smooth_loss_per_epoch(loss_per_epoch):
-    value = loss_per_epoch[0]
-    smooth_loss_per_epoch = []
-    momentum = .9
-    for i in loss_per_epoch:
-        value = momentum * value + (1 - momentum) * i
-        smooth_loss_per_epoch.append(value)
-    return smooth_loss_per_epoch
-
 def show_loss_graph(n_dim):
-    df = pd.read_csv("%s/outputs/classifier_%sD_training_logs.csv" % (folder_path, n_dim))
+    df = pd.read_csv("%s/outputs/classifier_%s_training_logs.csv" % (folder_path, n_dim))
     loss_per_epoch = df["loss"].values
+    smooth_loss_per_epoch = [np.mean(loss_per_epoch[max(0, i - 20) : min(len(loss_per_epoch), i + 20)]) for i in range(len(loss_per_epoch))]
 
     plt.figure(figsize=(18, 12))
     plt.plot(loss_per_epoch, color="orange")
-    plt.plot(get_smooth_loss_per_epoch(loss_per_epoch))
+    plt.plot(smooth_loss_per_epoch)
     plt.gcf().canvas.set_window_title("Loss per epoch")
     plt.title("Loss per epoch")
     plt.gca().axhline(y=.35, color="black", lw=1., alpha=.2)
@@ -31,12 +24,12 @@ def show_loss_graph(n_dim):
 # Read arguments from python command
 n_dim = None
 for param in sys.argv:
-    if param in ["2", "3"]:
+    if param in ["2D", "3D"]:
         n_dim = param
 
 if (n_dim == None):
     print("Usage: python show.loss_graph.py [n_dim]")
-    print("Use n_dim=2 to display the loss of the 2D classifier, n_dim=3 to display the loss of the 3D classifier")
+    print("Use n_dim=2D to display the loss of the 2D classifier, n_dim=3D to display the loss of the 3D classifier")
     exit(-1)
 
 show_loss_graph(n_dim)
