58c92ab633daf1b6d26656a8b8bdbbce04ecc885
Benoit Koenig <koenigbenoit47@gmail.com>
2019-10-06 10:38:42 +0800

    Add assertion error message when checking data consistency in 3D reconstruction

--- classification_3D/generate_3D_image.py
+++ classification_3D/generate_3D_image.py
@@ -9,17 +9,18 @@ from intracranial_hemorrhage_detection.preprocess import get_dicom_data
 def check_dicom_data_consistency(dicom_datas):
     "Given the dicom_data for a specific study, check the metadata consistency"
     dicom_data_0 = dicom_datas[0]
+    assertion_error_message = "Assertion error for instance id %s" % dicom_data_0["0020", "000d"][0:]
     for dicom_data in dicom_datas:
-        assert dicom_data["0020", "0032"][:2] == dicom_data_0["0020", "0032"][:2]
-        assert dicom_data["0020", "000d"] == dicom_data_0["0020", "000d"]
-        assert dicom_data["0020", "0037"] == dicom_data_0["0020", "0037"]
-        assert dicom_data["0028", "0030"] == dicom_data_0["0028", "0030"]
-        assert dicom_data["0028", "0010"] == dicom_data_0["0028", "0010"]
-        assert dicom_data["0028", "0011"] == dicom_data_0["0028", "0011"]
-        assert dicom_data["0028", "1050"] == dicom_data_0["0028", "1050"]
-        assert dicom_data["0028", "1051"] == dicom_data_0["0028", "1051"]
-        assert dicom_data["0028", "1052"] == dicom_data_0["0028", "1052"]
-        assert dicom_data.RescaleSlope == 1
+        assert dicom_data["0020", "0032"][:2] == dicom_data_0["0020", "0032"][:2], assertion_error_message
+        assert dicom_data["0020", "000d"] == dicom_data_0["0020", "000d"], assertion_error_message
+        assert dicom_data["0020", "0037"] == dicom_data_0["0020", "0037"], assertion_error_message
+        assert dicom_data["0028", "0030"] == dicom_data_0["0028", "0030"], assertion_error_message
+        assert dicom_data["0028", "0010"] == dicom_data_0["0028", "0010"], assertion_error_message
+        assert dicom_data["0028", "0011"] == dicom_data_0["0028", "0011"], assertion_error_message
+        assert dicom_data["0028", "1050"] == dicom_data_0["0028", "1050"], assertion_error_message
+        assert dicom_data["0028", "1051"] == dicom_data_0["0028", "1051"], assertion_error_message
+        assert dicom_data["0028", "1052"] == dicom_data_0["0028", "1052"], assertion_error_message
+        assert dicom_data.RescaleSlope == 1, assertion_error_message
 
 def generate_3D_image(slices_ids, folder="stage_1_train"):
     "All input slice ids must have the same study id. Outputs the voxels reconstructed for the given slices"
