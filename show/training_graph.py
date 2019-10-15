94ffd292b03de4274144bc595d9c9b26acb3e903
Benoit Koenig <koenigbenoit47@gmail.com>
2019-10-15 18:20:29 +0800

    Update show.training_graph to show val_loss instead of smooth_loss

--- show/training_graph.py
+++ show/training_graph.py
@@ -9,11 +9,11 @@ pd.plotting.register_matplotlib_converters()
 def show_training_graph():
     df = pd.read_csv(training_logs_file)
     loss_per_epoch = df["loss"].values
-    smooth_loss_per_epoch = [np.mean(loss_per_epoch[max(0, i - 20) : min(len(loss_per_epoch), i + 20)]) for i in range(len(loss_per_epoch))]
+    val_loss_per_epoch = df["val_loss"].values
 
     plt.figure(figsize=(18, 12))
     plt.plot(loss_per_epoch, color="orange")
-    plt.plot(smooth_loss_per_epoch)
+    plt.plot(val_loss_per_epoch)
     plt.gcf().canvas.set_window_title("Loss per epoch")
     plt.title("Loss per epoch")
     plt.gca().axhline(y=.3, color="black", lw=1., alpha=.2)
